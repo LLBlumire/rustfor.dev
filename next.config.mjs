b077a414027458a51e5c0288c134e271fe400eb2
lucy@llblumire.co.uk <lucy@llblumire.co.uk>
2023-06-24 13:49:31 +0100

    Fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,13 @@ import nextMDX from "@next/mdx";
 import remarkGfm from "remark-gfm";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ["ts", "tsx", "js", "jsx", "mdx"],
-  experimental: {
-    mdxRs: false,
-  },
-};
+const nextConfig = {};
 
 const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  remark: [remarkGfm],
-  rehype: []
+  options: {
+    remark: [remarkGfm],
+    rehype: [],
+  }
 });
 
 export default withMDX(nextConfig);
