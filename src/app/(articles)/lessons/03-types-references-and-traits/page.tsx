af591ab56245f194ee21ad9b02a5f0a7e4b6f980
lucy@llblumire.co.uk <lucy@llblumire.co.uk>
2023-06-26 15:30:54 +0100

    Add lesson 3

---
+++ src/app/(articles)/lessons/03-types-references-and-traits/page.tsx
@@ -0,0 +1,5 @@
+import L03TypesReferencesAndTraits from "~/components/markdown/lessons/L03TypesReferencesAndTraits.mdx";
+
+export default function Page() {
+  return <L03TypesReferencesAndTraits />;
+}
